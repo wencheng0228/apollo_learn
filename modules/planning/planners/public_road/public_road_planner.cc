0411024857f749434afbd261245327f5031459ef
wencheng.wang <wwencheng0228@gmail.com>
2025-07-19 16:06:58 +0800

    格式化

--- modules/planning/planners/public_road/public_road_planner.cc
+++ modules/planning/planners/public_road/public_road_planner.cc
@@ -25,44 +25,37 @@ namespace planning {
 using apollo::common::Status;
 using apollo::common::TrajectoryPoint;
 
-Status PublicRoadPlanner::Init(
-    const std::shared_ptr<DependencyInjector>& injector,
-    const std::string& config_path) {
-  Planner::Init(injector, config_path);
-  LoadConfig<PlannerPublicRoadConfig>(config_path, &config_);
-  scenario_manager_.Init(injector, config_);
-  return Status::OK();
+Status PublicRoadPlanner::Init(const std::shared_ptr<DependencyInjector>& injector, const std::string& config_path) {
+    Planner::Init(injector, config_path);
+    LoadConfig<PlannerPublicRoadConfig>(config_path, &config_);
+    scenario_manager_.Init(injector, config_);
+    return Status::OK();
 }
 
-Status PublicRoadPlanner::Plan(const TrajectoryPoint& planning_start_point,
-                               Frame* frame,
+Status PublicRoadPlanner::Plan(const TrajectoryPoint& planning_start_point, Frame* frame,
                                ADCTrajectory* ptr_computed_trajectory) {
-  scenario_manager_.Update(planning_start_point, frame);
-  scenario_ = scenario_manager_.mutable_scenario();
-  if (!scenario_) {
-    return Status(apollo::common::ErrorCode::PLANNING_ERROR,
-                  "Unknown Scenario");
-  }
-  auto result = scenario_->Process(planning_start_point, frame);
-
-  if (FLAGS_enable_record_debug) {
-    auto scenario_debug = ptr_computed_trajectory->mutable_debug()
-                              ->mutable_planning_data()
-                              ->mutable_scenario();
-    scenario_debug->set_scenario_plugin_type(scenario_->Name());
-    scenario_debug->set_stage_plugin_type(scenario_->GetStage());
-    scenario_debug->set_msg(scenario_->GetMsg());
-  }
-
-  if (result.GetScenarioStatus() == ScenarioStatusType::STATUS_DONE) {
-    // only updates scenario manager when previous scenario's status is
-    // STATUS_DONE
     scenario_manager_.Update(planning_start_point, frame);
-  } else if (result.GetScenarioStatus() == ScenarioStatusType::STATUS_UNKNOWN) {
-    return Status(common::PLANNING_ERROR,
-                  result.GetTaskStatus().error_message());
-  }
-  return Status(common::OK, result.GetTaskStatus().error_message());
+    scenario_ = scenario_manager_.mutable_scenario();
+    if (!scenario_) {
+        return Status(apollo::common::ErrorCode::PLANNING_ERROR, "Unknown Scenario");
+    }
+    auto result = scenario_->Process(planning_start_point, frame);
+
+    if (FLAGS_enable_record_debug) {
+        auto scenario_debug = ptr_computed_trajectory->mutable_debug()->mutable_planning_data()->mutable_scenario();
+        scenario_debug->set_scenario_plugin_type(scenario_->Name());
+        scenario_debug->set_stage_plugin_type(scenario_->GetStage());
+        scenario_debug->set_msg(scenario_->GetMsg());
+    }
+
+    if (result.GetScenarioStatus() == ScenarioStatusType::STATUS_DONE) {
+        // only updates scenario manager when previous scenario's status is
+        // STATUS_DONE
+        scenario_manager_.Update(planning_start_point, frame);
+    } else if (result.GetScenarioStatus() == ScenarioStatusType::STATUS_UNKNOWN) {
+        return Status(common::PLANNING_ERROR, result.GetTaskStatus().error_message());
+    }
+    return Status(common::OK, result.GetTaskStatus().error_message());
 }
 
 }  // namespace planning
--- modules/planning/scenarios/lane_follow/lane_follow_stage.cc
+++ modules/planning/scenarios/lane_follow/lane_follow_stage.cc
@@ -51,216 +51,193 @@ namespace {
 constexpr double kStraightForwardLineCost = 10.0;
 }  // namespace
 
-void LaneFollowStage::RecordObstacleDebugInfo(
-    ReferenceLineInfo* reference_line_info) {
-  if (!FLAGS_enable_record_debug) {
-    ADEBUG << "Skip record debug info";
-    return;
-  }
-  auto ptr_debug = reference_line_info->mutable_debug();
-
-  const auto path_decision = reference_line_info->path_decision();
-  for (const auto obstacle : path_decision->obstacles().Items()) {
-    auto obstacle_debug = ptr_debug->mutable_planning_data()->add_obstacle();
-    obstacle_debug->set_id(obstacle->Id());
-    obstacle_debug->mutable_sl_boundary()->CopyFrom(
-        obstacle->PerceptionSLBoundary());
-    const auto& decider_tags = obstacle->decider_tags();
-    const auto& decisions = obstacle->decisions();
-    if (decider_tags.size() != decisions.size()) {
-      AERROR << "decider_tags size: " << decider_tags.size()
-             << " different from decisions size:" << decisions.size();
+void LaneFollowStage::RecordObstacleDebugInfo(ReferenceLineInfo* reference_line_info) {
+    if (!FLAGS_enable_record_debug) {
+        ADEBUG << "Skip record debug info";
+        return;
     }
-    for (size_t i = 0; i < decider_tags.size(); ++i) {
-      auto decision_tag = obstacle_debug->add_decision_tag();
-      decision_tag->set_decider_tag(decider_tags[i]);
-      decision_tag->mutable_decision()->CopyFrom(decisions[i]);
-    }
-  }
-}
+    auto ptr_debug = reference_line_info->mutable_debug();
 
-StageResult LaneFollowStage::Process(
-    const TrajectoryPoint& planning_start_point, Frame* frame) {
-  if (frame->reference_line_info().empty()) {
-    return StageResult(StageStatusType::FINISHED);
-  }
-
-  bool has_drivable_reference_line = false;
-
-  ADEBUG << "Number of reference lines:\t"
-         << frame->mutable_reference_line_info()->size();
-
-  unsigned int count = 0;
-  StageResult result;
-  for (auto& reference_line_info : *frame->mutable_reference_line_info()) {
-    // TODO(SHU): need refactor
-    if (count++ == frame->mutable_reference_line_info()->size()) {
-      break;
-    }
-    ADEBUG << "No: [" << count << "] Reference Line.";
-    ADEBUG << "IsChangeLanePath: " << reference_line_info.IsChangeLanePath();
-
-    if (has_drivable_reference_line) {
-      reference_line_info.SetDrivable(false);
-      break;
-    }
-
-    result =
-        PlanOnReferenceLine(planning_start_point, frame, &reference_line_info);
-
-    if (!result.HasError()) {
-      if (!reference_line_info.IsChangeLanePath()) {
-        ADEBUG << "reference line is NOT lane change ref.";
-        has_drivable_reference_line = true;
-        continue;
-      }
-      if (reference_line_info.Cost() < kStraightForwardLineCost) {
-        // If the path and speed optimization succeed on target lane while
-        // under smart lane-change or IsClearToChangeLane under older version
-        has_drivable_reference_line = true;
-        reference_line_info.SetDrivable(true);
-      } else {
-        reference_line_info.SetDrivable(false);
-        ADEBUG << "\tlane change failed";
-      }
-    } else {
-      reference_line_info.SetDrivable(false);
-    }
-  }
-
-  return has_drivable_reference_line
-             ? result.SetStageStatus(StageStatusType::RUNNING)
-             : result.SetStageStatus(StageStatusType::ERROR);
-}
-
-StageResult LaneFollowStage::PlanOnReferenceLine(
-    const TrajectoryPoint& planning_start_point, Frame* frame,
-    ReferenceLineInfo* reference_line_info) {
-  if (!reference_line_info->IsChangeLanePath()) {
-    reference_line_info->AddCost(kStraightForwardLineCost);
-  }
-  ADEBUG << "planning start point:" << planning_start_point.DebugString();
-  ADEBUG << "Current reference_line_info is IsChangeLanePath: "
-         << reference_line_info->IsChangeLanePath();
-
-  StageResult ret;
-  for (auto task : task_list_) {
-    const double start_timestamp = Clock::NowInSeconds();
-    const auto start_planning_perf_timestamp =
-        std::chrono::duration<double>(
-            std::chrono::system_clock::now().time_since_epoch())
-            .count();
-
-    ret.SetTaskStatus(task->Execute(frame, reference_line_info));
-
-    const double end_timestamp = Clock::NowInSeconds();
-    const double time_diff_ms = (end_timestamp - start_timestamp) * 1000;
-    ADEBUG << "after task[" << task->Name()
-           << "]:" << reference_line_info->PathSpeedDebugString();
-    ADEBUG << task->Name() << " time spend: " << time_diff_ms << " ms.";
-    RecordDebugInfo(reference_line_info, task->Name(), time_diff_ms);
-
-    const auto end_planning_perf_timestamp =
-        std::chrono::duration<double>(
-            std::chrono::system_clock::now().time_since_epoch())
-            .count();
-    const auto plnning_perf_ms =
-        (end_planning_perf_timestamp - start_planning_perf_timestamp) * 1000;
-    AINFO << "Planning Perf: task name [" << task->Name() << "], "
-          << plnning_perf_ms << " ms.";
-
-    if (ret.IsTaskError()) {
-      AERROR << "Failed to run tasks[" << task->Name()
-             << "], Error message: " << ret.GetTaskStatus().error_message();
-      break;
-    }
-
-    // TODO(SHU): disable reference line order changes for now
-    // updated reference_line_info, because it is changed in
-    // lane_change_decider by PrioritizeChangeLane().
-    // reference_line_info = &frame->mutable_reference_line_info()->front();
-    // ADEBUG << "Current reference_line_info is IsChangeLanePath: "
-    //        << reference_line_info->IsChangeLanePath();
-  }
-
-  RecordObstacleDebugInfo(reference_line_info);
-
-  // check path and speed results for path or speed fallback
-  reference_line_info->set_trajectory_type(ADCTrajectory::NORMAL);
-  if (ret.IsTaskError()) {
-    fallback_task_->Execute(frame, reference_line_info);
-  }
-
-  DiscretizedTrajectory trajectory;
-  if (!reference_line_info->CombinePathAndSpeedProfile(
-          planning_start_point.relative_time(),
-          planning_start_point.path_point().s(), &trajectory)) {
-    const std::string msg = "Fail to aggregate planning trajectory.";
-    AERROR << msg;
-    return ret.SetStageStatus(StageStatusType::ERROR, msg);
-  }
-
-  // determine if there is a destination on reference line.
-  double dest_stop_s = -1.0;
-  for (const auto* obstacle :
-       reference_line_info->path_decision()->obstacles().Items()) {
-    if (obstacle->LongitudinalDecision().has_stop() &&
-        obstacle->LongitudinalDecision().stop().reason_code() ==
-            STOP_REASON_DESTINATION) {
-      SLPoint dest_sl = GetStopSL(obstacle->LongitudinalDecision().stop(),
-                                  reference_line_info->reference_line());
-      dest_stop_s = dest_sl.s();
-    }
-  }
-
-  for (const auto* obstacle :
-       reference_line_info->path_decision()->obstacles().Items()) {
-    if (obstacle->IsVirtual()) {
-      continue;
-    }
-    if (!obstacle->IsStatic()) {
-      continue;
-    }
-    if (obstacle->LongitudinalDecision().has_stop()) {
-      bool add_stop_obstacle_cost = false;
-      if (dest_stop_s < 0.0) {
-        add_stop_obstacle_cost = true;
-      } else {
-        SLPoint stop_sl = GetStopSL(obstacle->LongitudinalDecision().stop(),
-                                    reference_line_info->reference_line());
-        if (stop_sl.s() < dest_stop_s &&
-            (dest_stop_s - reference_line_info->AdcSlBoundary().end_s()) <
-                20.0) {
-          add_stop_obstacle_cost = true;
+    const auto path_decision = reference_line_info->path_decision();
+    for (const auto obstacle : path_decision->obstacles().Items()) {
+        auto obstacle_debug = ptr_debug->mutable_planning_data()->add_obstacle();
+        obstacle_debug->set_id(obstacle->Id());
+        obstacle_debug->mutable_sl_boundary()->CopyFrom(obstacle->PerceptionSLBoundary());
+        const auto& decider_tags = obstacle->decider_tags();
+        const auto& decisions = obstacle->decisions();
+        if (decider_tags.size() != decisions.size()) {
+            AERROR << "decider_tags size: " << decider_tags.size()
+                   << " different from decisions size:" << decisions.size();
         }
-      }
-      if (add_stop_obstacle_cost) {
-        static constexpr double kReferenceLineStaticObsCost = 1e3;
-        reference_line_info->AddCost(kReferenceLineStaticObsCost);
-      }
+        for (size_t i = 0; i < decider_tags.size(); ++i) {
+            auto decision_tag = obstacle_debug->add_decision_tag();
+            decision_tag->set_decider_tag(decider_tags[i]);
+            decision_tag->mutable_decision()->CopyFrom(decisions[i]);
+        }
     }
-  }
-
-  if (FLAGS_enable_trajectory_check) {
-    if (ConstraintChecker::ValidTrajectory(trajectory) !=
-        ConstraintChecker::Result::VALID) {
-      const std::string msg = "Current planning trajectory is not valid.";
-      AERROR << msg;
-      return ret.SetStageStatus(StageStatusType::ERROR, msg);
-    }
-  }
-
-  reference_line_info->SetTrajectory(trajectory);
-  reference_line_info->SetDrivable(true);
-  ret.SetStageStatus(StageStatusType::RUNNING);
-  return ret;
 }
 
-SLPoint LaneFollowStage::GetStopSL(const ObjectStop& stop_decision,
-                                   const ReferenceLine& reference_line) const {
-  SLPoint sl_point;
-  reference_line.XYToSL(stop_decision.stop_point(), &sl_point);
-  return sl_point;
+// 车道跟随的处理
+StageResult LaneFollowStage::Process(const TrajectoryPoint& planning_start_point, Frame* frame) {
+    if (frame->reference_line_info().empty()) {
+        return StageResult(StageStatusType::FINISHED);
+    }
+
+    bool has_drivable_reference_line = false;
+
+    ADEBUG << "Number of reference lines:\t" << frame->mutable_reference_line_info()->size();
+
+    unsigned int count = 0;
+    StageResult result;
+    for (auto& reference_line_info : *frame->mutable_reference_line_info()) {
+        // TODO(SHU): need refactor
+        if (count++ == frame->mutable_reference_line_info()->size()) {
+            break;
+        }
+        ADEBUG << "No: [" << count << "] Reference Line.";
+        ADEBUG << "IsChangeLanePath: " << reference_line_info.IsChangeLanePath();
+
+        if (has_drivable_reference_line) {
+            reference_line_info.SetDrivable(false);
+            break;
+        }
+
+        result = PlanOnReferenceLine(planning_start_point, frame, &reference_line_info);
+
+        if (!result.HasError()) {
+            if (!reference_line_info.IsChangeLanePath()) {
+                ADEBUG << "reference line is NOT lane change ref.";
+                has_drivable_reference_line = true;
+                continue;
+            }
+            if (reference_line_info.Cost() < kStraightForwardLineCost) {
+                // If the path and speed optimization succeed on target lane while
+                // under smart lane-change or IsClearToChangeLane under older version
+                has_drivable_reference_line = true;
+                reference_line_info.SetDrivable(true);
+            } else {
+                reference_line_info.SetDrivable(false);
+                ADEBUG << "\tlane change failed";
+            }
+        } else {
+            reference_line_info.SetDrivable(false);
+        }
+    }
+
+    return has_drivable_reference_line ? result.SetStageStatus(StageStatusType::RUNNING)
+                                       : result.SetStageStatus(StageStatusType::ERROR);
+}
+
+StageResult LaneFollowStage::PlanOnReferenceLine(const TrajectoryPoint& planning_start_point, Frame* frame,
+                                                 ReferenceLineInfo* reference_line_info) {
+    if (!reference_line_info->IsChangeLanePath()) {
+        reference_line_info->AddCost(kStraightForwardLineCost);
+    }
+    ADEBUG << "planning start point:" << planning_start_point.DebugString();
+    ADEBUG << "Current reference_line_info is IsChangeLanePath: " << reference_line_info->IsChangeLanePath();
+
+    StageResult ret;
+    for (auto task : task_list_) {
+        const double start_timestamp = Clock::NowInSeconds();
+        const auto start_planning_perf_timestamp =
+            std::chrono::duration<double>(std::chrono::system_clock::now().time_since_epoch()).count();
+
+        ret.SetTaskStatus(task->Execute(frame, reference_line_info));
+
+        const double end_timestamp = Clock::NowInSeconds();
+        const double time_diff_ms = (end_timestamp - start_timestamp) * 1000;
+        ADEBUG << "after task[" << task->Name() << "]:" << reference_line_info->PathSpeedDebugString();
+        ADEBUG << task->Name() << " time spend: " << time_diff_ms << " ms.";
+        RecordDebugInfo(reference_line_info, task->Name(), time_diff_ms);
+
+        const auto end_planning_perf_timestamp =
+            std::chrono::duration<double>(std::chrono::system_clock::now().time_since_epoch()).count();
+        const auto plnning_perf_ms = (end_planning_perf_timestamp - start_planning_perf_timestamp) * 1000;
+        AINFO << "Planning Perf: task name [" << task->Name() << "], " << plnning_perf_ms << " ms.";
+
+        if (ret.IsTaskError()) {
+            AERROR << "Failed to run tasks[" << task->Name()
+                   << "], Error message: " << ret.GetTaskStatus().error_message();
+            break;
+        }
+
+        // TODO(SHU): disable reference line order changes for now
+        // updated reference_line_info, because it is changed in
+        // lane_change_decider by PrioritizeChangeLane().
+        // reference_line_info = &frame->mutable_reference_line_info()->front();
+        // ADEBUG << "Current reference_line_info is IsChangeLanePath: "
+        //        << reference_line_info->IsChangeLanePath();
+    }
+
+    RecordObstacleDebugInfo(reference_line_info);
+
+    // check path and speed results for path or speed fallback
+    reference_line_info->set_trajectory_type(ADCTrajectory::NORMAL);
+    if (ret.IsTaskError()) {
+        fallback_task_->Execute(frame, reference_line_info);
+    }
+
+    DiscretizedTrajectory trajectory;
+    if (!reference_line_info->CombinePathAndSpeedProfile(planning_start_point.relative_time(),
+                                                         planning_start_point.path_point().s(), &trajectory)) {
+        const std::string msg = "Fail to aggregate planning trajectory.";
+        AERROR << msg;
+        return ret.SetStageStatus(StageStatusType::ERROR, msg);
+    }
+
+    // determine if there is a destination on reference line.
+    double dest_stop_s = -1.0;
+    for (const auto* obstacle : reference_line_info->path_decision()->obstacles().Items()) {
+        if (obstacle->LongitudinalDecision().has_stop() &&
+            obstacle->LongitudinalDecision().stop().reason_code() == STOP_REASON_DESTINATION) {
+            SLPoint dest_sl = GetStopSL(obstacle->LongitudinalDecision().stop(), reference_line_info->reference_line());
+            dest_stop_s = dest_sl.s();
+        }
+    }
+
+    for (const auto* obstacle : reference_line_info->path_decision()->obstacles().Items()) {
+        if (obstacle->IsVirtual()) {
+            continue;
+        }
+        if (!obstacle->IsStatic()) {
+            continue;
+        }
+        if (obstacle->LongitudinalDecision().has_stop()) {
+            bool add_stop_obstacle_cost = false;
+            if (dest_stop_s < 0.0) {
+                add_stop_obstacle_cost = true;
+            } else {
+                SLPoint stop_sl =
+                    GetStopSL(obstacle->LongitudinalDecision().stop(), reference_line_info->reference_line());
+                if (stop_sl.s() < dest_stop_s && (dest_stop_s - reference_line_info->AdcSlBoundary().end_s()) < 20.0) {
+                    add_stop_obstacle_cost = true;
+                }
+            }
+            if (add_stop_obstacle_cost) {
+                static constexpr double kReferenceLineStaticObsCost = 1e3;
+                reference_line_info->AddCost(kReferenceLineStaticObsCost);
+            }
+        }
+    }
+
+    if (FLAGS_enable_trajectory_check) {
+        if (ConstraintChecker::ValidTrajectory(trajectory) != ConstraintChecker::Result::VALID) {
+            const std::string msg = "Current planning trajectory is not valid.";
+            AERROR << msg;
+            return ret.SetStageStatus(StageStatusType::ERROR, msg);
+        }
+    }
+
+    reference_line_info->SetTrajectory(trajectory);
+    reference_line_info->SetDrivable(true);
+    ret.SetStageStatus(StageStatusType::RUNNING);
+    return ret;
+}
+
+SLPoint LaneFollowStage::GetStopSL(const ObjectStop& stop_decision, const ReferenceLine& reference_line) const {
+    SLPoint sl_point;
+    reference_line.XYToSL(stop_decision.stop_point(), &sl_point);
+    return sl_point;
 }
 
 }  // namespace planning
